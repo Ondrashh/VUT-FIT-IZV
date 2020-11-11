a0ba4fd92d70293b55635969f63f1eec242ca16c
Ondrashh <55767000+Ondrashh@users.noreply.github.com>
2020-11-11 12:29:41 +0100

    Add files via upload

--- download.py
+++ download.py
@@ -42,18 +42,24 @@ class DataDownloader:
         self.folder = folder
         self.cache_filename = cache_filename
 
-    # funkce stáhne do datové složky folder všechny soubory s daty z adresy url.
+    # Funkce stáhne do datové složky folder všechny soubory s daty z adresy url.
     # Funkce po stáhnutí zip souborů z přiřazené URL adresy přečte 
     def download_data(self):
 
+        # GET request na získání obsahu stránky, přidána hlavička aby nás server neodmítnul
         r = requests.get("https://ehw.fit.vutbr.cz/izv/", headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"},allow_redirects=True)
 
+        # Rozparsování html souboru, abych mohli přistupovat k elementům
         result = BeautifulSoup(r.text, "html.parser")
+        # Chceme získat tak buttonu, protože pod ním se skrývá odkaz na zip soubory
         zip_html_tag = result.findAll(class_='btn btn-sm btn-primary')
 
+        # Pokud složka do které chceme importovat neexistuje, tak ji vytvoříme
         if not os.path.exists(self.folder):
             os.makedirs(self.folder)
+        # Pro každý odkaz stáhneme soubory abychom je mohli zpacovat
         for item in zip_html_tag:
+            # Tady probíhá získávání odkazu a následné stahování zip souborů do složky folder
             url = item.get('href')
             link_to_download = 'https://ehw.fit.vutbr.cz/izv/' + url
             just_zip_name = url.split("/")
@@ -61,184 +67,100 @@ class DataDownloader:
             with open(self.folder + '/' + just_zip_name[1], 'wb') as fd:
                 fd.write(download_request.content)
 
-
-    """nejsou data pro daný kraj stažená, stáhne je do datové složky folder. Poté
-    je pro daný region specifikovaný tříznakovým kódem (viz tabulka níže) vždy
-    vyparsuje do následujícího formátu dvojice (tuple), kde první položka je seznam
-    (list) řetězců a druhá položka bude seznam (list) NumPy polí, schematicky:
-    tuple(list[str], list[np.ndarray])
-    Seznam řetězců odpovídá názvům jednotlivých datových sloupců, NumPy pole
-    budou obsahovat data. Platí, že délka obou seznamů je stejná, shape všech
-    NumPy polí je stejný. Při parsování přidejte nový sloupec “region”, který bude
-    obsahovat tříznaký kód patřičného kraje, tj. odpovídá hodnotě region. Pro každý
-    sloupec zvolte vhodný datový typ (t.j. snažte se vyhnout textovým řetězcům,
-    vyřešte desetinnou čárku atp.). """
+    # Tato funkce rozparsuje tada z jednotlivých zip souborů pro kraje, tak abychom je mohli dále použít
+    # Metoda je primárně volána pomocí get_list()
+    # Argumenty: region - jeden kraj, který zpracuje
+    # returns: tuple(list[str_nazvy_sloupcu], list[np.ndarrays_data sloupců])
     def parse_region_data(self, region):
+        # Pokud složka neexistuje, musím si ji vytvořit a stáhnout všechny soubory
+        # Viz. zadání
         if not os.path.exists(self.folder):
             os.makedirs(self.folder)
+            # Je volána metoda download_data()
             self.download_data()
+
+        # Definice první části tuple, definovány názvy datových sloupců
         data_head = ["p1", "p36", "p37", "p2a", "weekday(p2a)", "p2b", "p6", "p7", "p8", "p9", "p10", "p11", "p12",
                          "p13a", "p13b", "p13c", "p14", "p15", "p16", "p17", "p18", "p19", "p20", "p21", "p22", "p23",
                          "p24", "p27", "p28", "p34", "p35", "p39", "p44", "p45a", "p47", "p48a", "p49", "p50a", "p50b",
                          "p51", "p52", "p53", "p55a", "p57", "p58", "a", "b", "d", "e", "f", "g", "h", "i", "j", "k",
                          "l", "n", "o", "p", "q", "r", "s", "t", "p5a", "region"]
 
-        # result_tuple = tuple(data_head, list[np.ndarray])
-        lines_count = 0
+        # Definuji si měsíc, protože dynamicky hledám poslední měsíc v roce 2020
         month = 0
         try:
+            # Pro každou položku ve složce folder hledám příslušný soubor pro konec roku
             for file in os.listdir(self.folder):
-                # print(self.folder + '/' + self.Regions[region].value)
-                #print(file)
-
+                # Získání celé adresy
                 current_file = self.folder + '/' + file
+                # Rozparsování názvu zip souborů abych mohl najít poslední soubor v roce 2020
                 rok_zip = current_file.rsplit('-', 2)
                 if(file != "datagis2016.zip"):
                     if(len(rok_zip) > 2):
                         if(rok_zip[2] == "2020.zip"):
                             if(int(month) < int(rok_zip[1])):
+                                #Získám si poslední měsíc v roce 2020
                                 month = rok_zip[1]
-
+            # Sestavení názvu souboru s daty posledního měsíce v roce 2020
             year2020 = "datagis-" + month + "-2020.zip"
+            # Seznam souborů definující konce jednotlivých roků a podle nich vyhledávám data
             files_to_extract = ["datagis2016.zip", "datagis-rok-2017.zip", "datagis-rok-2018.zip",
                                "datagis-rok-2019.zip", year2020]
 
-            """item_array = np.ndarray(1)
-            item_array = np.append(kokos, item_array)
-            print(files_to_extract)
-            for file in files_to_extract:
-                current_zip = ZipFile(self.folder + '/' + file)
-                with current_zip.open(self.Regions[region].value, 'r') as csvfile:
-                    current_csv = list(csv.reader(TextIOWrapper(csvfile, encoding='windows-1250')))
-                    # print(current_csv[1][0])
-                    var = sum(1 for _ in current_csv)
-                    lines_count += var
-                    print(lines_count)
-                    # item_array = np.append()
-                    # for line in current_csv:
-                    #  print(line)
-            # print(item_array)"""
-
-            cols = []
-            for j in range(0, 6):
-                if(j == 3 ):
-                    a = np.array([], dtype=str)
-                else:
-                    a = np.array([], dtype=int)
-                cols.append(a)
+            # Vytvoření pomocného listu, abych mohl přetypovávat hodnoty jednotlivých řádků
             test = [None]*65
-            result_array =  []
+            # Definice výsledného array, kam poté appenduji jednotlivé řádky
+            result_array = []
+            # Cyklus na průchod souborů konců roků a následná extrakce řádků, které budu poté potřebovat
             for file in files_to_extract:
                 with ZipFile(self.folder + '/' + file, 'r') as zip:
                     with zip.open(self.Regions[region].value) as file:
                         reader = csv.reader(TextIOWrapper(file, "windows-1250",),delimiter=';')
+                        # Potřebuji projít každý řádek v souboru
                         for row in reader:
-                            #print(row[0])
                             iter = 0
+                            # Tento cyklus slouží k průchodu všech prvků řádku abych je přetypovat
                             for i in row:
                                 try:
+                                    # Vím že na této pozici se nachází datum, které přetypuji na datetime formát
                                     if(iter == 3):
                                         test[iter] = datetime.strptime(i, "%Y-%m-%d")
-                                        #print(test[iter].year)
+                                    # Následně zkusím přetypovat content na číslo, protože většina obsahu jsou čísla
                                     else:
                                         test[iter] = int(i)
                                     iter += 1
+                                # Pokud je obsah nic, nebo string tak se přiřadí typem string
                                 except ValueError:
                                     test[iter] = i
                                     iter += 1
+                            # Přiřazení regionu na konec viz. zadání
                             test[64] = region
-                            #print(test[0])
-                            # print(row[0])
-                            # print(np.shape(row)
-                            #print(np.shape(row))
-                            #row = np.transpose(row)
-                            #test = np.append(test, [region])
-                            conv = int(row[0])
-                            #print(type(conv))
-                            #row[0] = conv
-                            #print(type(row[0]))
-                            #print(type(int(row[0])))
-
+                            # Append s výskedným arrayem, které symbolizuje výsledek
                             result_array.append(test)
 
-
-
-                            # print(data)
-                            # data = data.decode("windows-1250")
-                            # data = data.replace('"', "")
-                            # array = []
-                            # var = np.transpose(data)
-                            # print(var)
-                            # print(data.count())
-                            # data = data.replace(',', '.')
-                            """for d in data.splitlines():
-                                #print(d)
-                                d = d.split(';')
-                                d = np.transpose(d)
-                                d = np.append(d, [region])"""
-                            # print(d[0])
-                        #print(d[63])
-                        #array.append(d)
-                        #print(np.shape(d))
-                        #print(d[0][1])
-
-                        #print(d)
-                        #print(np.shape(d))
-                        # print(d)
-                        # print(np.shape(d))
-                        #d = np.transpose(d)
-                        #print(np.shape(d))
-                        #print(np.shape(d))
-
-                        # print(np.shape(d))
-                        # print(np.shape(result_array))
-                        #result_array.append(d)
-                        #dalsi = result_array
-                        #dalsi = np.transpose(dalsi)
-                        # print(dalsi)
-                        #print(np.shape(result_array))
-            # print(np.shape(result_array[500]))
-            # print(cols)
-            #print(np.shape(result_array))
-            # print(result_array[-1])
-            # print(np.shape(result_array))
-            #colums_rule = np.transpose(result_array)
-            #print(np.shape(colums_rule))
-            #print(cols[0])
-                # print(colums_rule[j])
-
-            #print(result_array[0])
-            print("Parser did it, mom get the camera")
-            """for item in result_array:
-                for j in range(0, 64):
-                    a = np.array([])
-                    cols.append(a)"""
-
+            # Velmi důležitá část, měním závislost řádku a sloupců, protože když to otočím
+            # můžu jednododuše získat jednotlivé sloupce s daty
             result_array= np.transpose(result_array)
-            #kokos = result_array[0]
-            #print(type(kokos))
-            #kokos1 = (map(int, kokos))
-            #print(type(kokos1))
-            #result_array[0] = list(map(int, result_array[0]))
-            #print(type(result_array[0][0]))
-            #print(result_array)
-            #print(np.shape(result_array))
-            # pr
+
+            # Vytvoření výsledné dvojice (tuple)
             var = (data_head, result_array)
-            #print(var)
+
+            # Funkce vrací tuple(list[str], list[np.ndarray])
+            # Viz. zadání
             return var
 
+        # Probíhá kontrola, jestli je vůbec zadán validní kraj
         except KeyError:
             print("Not a valid region!")
             exit(101)
 
-
-    """ get_list(self, regions = None)
-    Vrací zpracovaná data pro vybrané kraje (regiony). Argument regions specifikuje
-    seznam (list) požadovaných krajů jejich třípísmennými kódy. Pokud seznam není
-    uveden (je použito None), zpracují se všechny kraje včetně Prahy. Výstupem funkce
-    je dvojice ve stejném formátu, jako návratová hodnota funkce """
+    # Funkce vrací zpracovaná dat pro kraje, které jsou mu zadané
+    # Pro zpracování volá metodu parse_region_data !
+    # Argumenty: regions - seznam regionů, které zpracovává (None = všechny)
+    # returns: tuple(list[str_nazvy_sloupcu], list[np.ndarrays_data sloupců])
+    # V podstatě vrací to stejné co parse_region_data, ale rozšířené
     def get_list(self, regions=None):
+        # Vytvoření arraye abychom mohli získat výsledky ze souboru
         cols = []
         for j in range(0, 65):
             if (j == 3):
@@ -246,65 +168,54 @@ class DataDownloader:
             else:
                 a = np.array([], dtype=int)
             cols.append(a)
+
+        # Pokud není zadán žádný kraj bereme všechny viz. zadání
         if (regions == None):
             regions = []
             for region in self.Regions:
                 regions.append(region.name)
-            print(regions)
-        get_list_result = []
+        # Cyklus se provede pro všechny zadané regiony
+        # Dělí se na:
+        # 1) Pokud je soubor s daty kraje už uložen - otevře ho a data vezme z něj a vrátí je
+        # 2) Pokud není uložen tak zavolá metodu parse_region_data a následně vytvoří soubor a vrátí hodnotu
         for region in regions:
             try:
+                # Získá si název cache souboru jak by se měl jmenovat
                 cache_file_name = self.cache_filename.replace("{}", region)
+                # Pokusí se ho otevřit
                 with gzip.open(cache_file_name, 'rb') as f:
-                    print(region, " : Beru ze souboru")
+                    # Pomocí pickle se vytáhne data
                     datas = pickle.load(f)
+                    # Přiřazení do listu polí
                     for i in range(0, 65):
                         cols[i] = np.append(cols[i], datas[1][i])
 
-
+            # Pokud cache file neexistuje, tak si ho vytvoříme
             except FileNotFoundError:
-                print(region, " : Parsuju sam")
+                # Vytvoření názvu cache souboru
                 cache_file_name = self.cache_filename.replace("{}", region)
+                # Získání dat o regionu pomocí metody parse_region_data
                 datas = self.parse_region_data(region)
+                # Přiřazení do pomocného listu polí
                 for i in range(0, 65):
                     cols[i] =  np.append(cols[i], datas[1][i])
-                """with gzip.open(cache_file_name, 'wb') as f:
+                # Vytvoření cache souboru s daty pr odalší použití
+                with gzip.open(cache_file_name, 'wb') as f:
                      pickle.dump(datas, f)
-                     f.close()"""
-                #print(cols)
-                #print(cols[64])
-                print("Delam to pro tenhle:", region)
-                #print(datas[0])
-                #print(get_list_result)
+                     f.close()
+
+        # Vytvoření dvojice (tuple), abychom ji mohli vrátit
         list_of_all = (datas[0], cols)
-        # print(type(datas[1]))
-        #rint(cols)
-        #print(np.shape(cols))
+
+        # Vrací tuple(list[str_nazvy_sloupcu], list[np.ndarrays_data sloupců])
         return list_of_all
-"""Pro každý kraj získá data s využitím funkce parse_region_data tak, že se
-budou výsledky uchovávat v paměti (v nějakém atributu instance třídy) a ukládat do
-pomocného cache souboru pomocí následujícího schématu:
-○ pokud už je výsledek načtený v paměti (tj. dostupný ve vámi zvoleném
-atributu), vrátí tuto dočasnou kopii
-○ pokud není uložený v paměti, ale je již zpracovaný v cache souboru, tak
-načte výsledek z cache, uloží jej do atributu a vrátí.
-○ jinak se zavolá funkce parse_region_data, výsledek volání se
-uloží do cache, poté do paměti a výsledek vrátí
-Pokud bude skript spuštěný jako hlavní (t.j. nebude importovaný jako modul)
-1
-pro 3 vámi vybrané kraje, stáhněte data
- (s využitím funkce get_list) a vypište do konzole základní
-informace o stažených datech (jaké jsou sloupce, počet záznamů a jaké kraje jsou v
-datasetu).
-"""
 
-
-# Press the green button in the gutter to run the script.
+# Main, pokud bude skript spuštěn samostatně vyberu si 3 kraje a rozparsuji data
 if __name__ == '__main__':
-    print('PyCharm')
-    kokos = DataDownloader()
-    # kokos.download_data()
-    # damn  = kokos.parse_region_data("JHM")
-    kokos.get_list(["VYS"])
-    # print(damn)
+    # Vypsání základních informací a zpracování předdefinovaných dat
+    print("Spuštění skriptu jako hlavní: načítání dat...")
+    data = DataDownloader().get_list(["VYS", "JHM", "OLK"])
+    print("Sloupce:", data[0])
+    print("Počet sloupců a množství záznamů:", np.shape(data[1]))
+    print("Kraje které jsem vybral: VYS, JHM, OLK")
 
--- get_stat.py
+++ get_stat.py
@@ -1,22 +1,27 @@
-import requests
-import os
-import csv
-import pickle
-import gzip
-from bs4 import BeautifulSoup
-from enum import Enum
-from zipfile import ZipFile
-from io import TextIOWrapper
-import numpy as np
+# Projekt do předmětu IZV, První část
+# Zpracoval: Ondřej Pavlacký (xpavla15) 3BIT
+# Implementace modulu get_stat.py
+# Vytvořeno: 11.11.2020
+# VUT FIT
+
+import matplotlib.pyplot as plt
 
 from IZV.IZV.download import DataDownloader
 
 
 def plot_stat(data_source, fig_location = None, show_figure = False):
-    print(data_source[1])
-
+    # data_source = DataDownloader().get_list(["VYS", "JHM"])
+    # print(type(data_source[1][0][0]))
+    for zaznam in data_source[1]:
+        
+    fig = plt.figure(figsize=(8, 4))
+    ax1 = fig.add_axes((0.1, 0.1, 0.8, 0.8))
+    ax2 = fig.add_axes((0.6, 0.6, 0.25, 0.25))
+    ax1.plot([0, 1], [1, 0], color='C3')
+    ax2.plot([0, 1], [0, 1], color='#35495e')
+    plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data_source = DataDownloader().get_list(["VYS","JHM"])
+    data_source = DataDownloader().get_list(["VYS", "JHM", "OLK"])
     plot_stat(data_source)

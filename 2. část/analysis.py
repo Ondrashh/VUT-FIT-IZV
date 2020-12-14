31b87b191188f743eaa6fb7e5c48200127e35c43
Ondrashh <xpavla15@vutbr.cz>
2020-12-14 20:44:06 +0100

    peepo sad

--- 2. část/analysis.py
+++ 2. část/analysis.py
@@ -39,10 +39,51 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
         # Změněn přistup - předtím jsem promazával data a měnil typy
         # teď mě stačí vlastně jen vzít sloupce, které mě zajímají a u nich upravit dat. typy
         # Doufám, že je to správný postup
-        datas = datas[["p2b", "p2a", "p13a", "p13b", "p13c", "region", "p12", "p14", "p53"]]
+        # (nakonec jen přetypovávám, nevím jestli, když je nezahrnu není chyba
 
         # Změny typů z obj. na int
-        datas["p2b"] = datas["p2b"].astype("int8")
+        datas["p1"] = datas["p1"].astype("float")
+        datas["p36"] = datas["p36"].astype("int8")
+        datas["p6"] = datas["p6"].astype("int8")
+        datas["p7"] = datas["p7"].astype("int8")
+        datas["p8"] = datas["p8"].astype("int8")
+        datas["p8"] = datas["p8"].astype("int8")
+        datas["p9"] = datas["p9"].astype("int8")
+        datas["p10"] = datas["p10"].astype("int8")
+        datas["p11"] = datas["p11"].astype("int8")
+        datas["p15"] = datas["p15"].astype("int8")
+        datas["p16"] = datas["p16"].astype("int8")
+        datas["p17"] = datas["p17"].astype("int8")
+        datas["p18"] = datas["p18"].astype("int8")
+        datas["p19"] = datas["p19"].astype("int8")
+        datas["p20"] = datas["p20"].astype("int8")
+        datas["p21"] = datas["p21"].astype("int8")
+        datas["p22"] = datas["p22"].astype("int8")
+        datas["p23"] = datas["p23"].astype("int8")
+        datas["p24"] = datas["p24"].astype("int8")
+        datas["p27"] = datas["p27"].astype("int8")
+        datas["p28"] = datas["p28"].astype("int8")
+        datas["p34"] = datas["p34"].astype("int8")
+        datas["p35"] = datas["p35"].astype("int8")
+        datas["p39"] = datas["p39"].astype("int8")
+        datas["p44"] = datas["p44"].astype("int8")
+        datas["p45a"] = datas["p45a"].astype("int8")
+        datas["p47"] = datas["p47"].astype("int8")
+        datas["p48a"] = datas["p48a"].astype("int8")
+        datas["p49"] = datas["p49"].astype("int8")
+        datas["p50a"] = datas["p50a"].astype("int8")
+        datas["p50a"] = datas["p50a"].astype("int8")
+        datas["p50b"] = datas["p50b"].astype("int8")
+        datas["p51"] = datas["p51"].astype("int8")
+        datas["p52"] = datas["p52"].astype("int8")
+        datas["p53"] = datas["p53"].astype("int8")
+        datas["p55a"] = datas["p55a"].astype("int8")
+        datas["p57"] = datas["p57"].astype("int8")
+        datas["p58"] = datas["p58"].astype("int8")
+
+
+
+        datas["weekday(p2a)"] = datas["weekday(p2a)"].astype("int8")
         datas["p13a"] = datas["p13a"].astype("int8")
         datas["p13b"] = datas["p13b"].astype("int8")
         datas["p13c"] = datas["p13c"].astype("int8")
@@ -117,16 +158,22 @@ def plot_conseq(df: pd.DataFrame, fig_location: str = None,
 # Ukol3: příčina nehody a škoda
 def plot_damage(df: pd.DataFrame, fig_location: str = None,
                 show_figure: bool = False):
+
     df["range"] = pd.cut(df["p14"], [-1, 50, 200, 500, 1000, float("inf")], labels=["<50", "50-200", "200-500", "500-1000", ">1000"])
     df["p12"] = pd.cut(df["p12"], [0, 201, 301, 401, 501, 601, 1000], labels=["nezaviněná řidičem", "nepřiměřená rychlost jízdy", "nesprávné předjíždění", "nedání přednosti v jízdě", "nesprávný způsob jízdy", "technická závada vozidla"])
     grouped = df[["region", "p12", "p2b", "p14", "range"]].groupby(["region", "p12", "range"], as_index=False).agg({"p2b": "count"})
     region_HKK = grouped[grouped["region"] == "HKK"]
-    region_JMK = grouped[grouped["region"] == "JMK"]
+    region_JHM = grouped[grouped["region"] == "JHM"]
     region_OLK = grouped[grouped["region"] == "OLK"]
     region_OST = grouped[grouped["region"] == "PAK"]
 
-    
+    fig, axes = plt.subplots(2, 2, figsize=(8, 8))
+    ax = axes.flatten()
 
+    # Vytvoření titulku
+    fig.suptitle('Úkol 3. - regiony', fontsize=20)
+    # Použití barplotu podle specifikace v zadání = sloupcové grafy
+    sns.barplot(data=region_HKK, ax=ax[0])
 
     # Uložení obrázku podle podmínky, kotrola adresáře a při neexistujícím, vytvoření nového
     # Musím prvně ukládat, protože jinak se to neuloží
